af1b6ea1f7422248a821888c478cfa193509407f
wx1458084829 <15328400570@163.com>
2022-04-09 00:57:17 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<el-upload
+    list-type="picture-card"
+    action="http://127.0.0.1:3000/up-file/"
+    multiple
+    :limit="3"
+    name = "file-name"
+    :on-success="handleSucess"
+  >
+    <el-button type="primary">Click to upload</el-button>
+    <template #tip>
+      <div class="el-upload__tip">
+        jpg/png files with a size less than 500KB.
+      </div>
+    </template>
+  </el-upload>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+  },data() {
+    return {
+      
+    }
+  },methods:{
+handleSucess(response,uploadFile,uploadFiles){
+  console.log(response);
+}
   }
 }
 </script>
